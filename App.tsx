585390d78f60d4793546898595d1ad6e17f304e0
Vatsal Sakariya <vatsal.sakariya@toddleapp.com>
2023-04-22 12:08:30 +0530

    style(prettier): add prettier configuration file refactor(App.js): delete App.js and create App.tsx feat(app.json): add eas configuration feat(package.json): add typescript and prettier-plugin-sort-imports as dev dependencies feat(tsconfig.json): add baseUrl and paths configuration to support absolute imports

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, StyleSheet, Text } from "react-native"
+import { StatusBar } from "expo-status-bar"
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar style="auto" />
+      <Text>Hello, World!</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
